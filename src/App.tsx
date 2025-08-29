bff7189ff19b849405ed8ecec0e76f30788f90e2
gotlnwza182 <6131501073@lamdaun.mfu.ac.th>
2025-08-29 15:24:20 +0700

    add all files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <div className="bg-cream h-screen flex items-center justify-center text-sm">
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card.tsx
@@ -0,0 +1,37 @@
+const Card = () => {
+  return (
+    <div className="bg-white rounded-lg grid grid-cols-1 md:grid-cols-2 w-[21.875rem] h-[46.875rem] md:w-[37.5rem] md:h-[28.5rem]">
+      <img
+        src="/image-product-mobile.jpg"
+        alt="product mobile"
+        className="max-w-full h-auto rounded-t-lg md:hidden"
+      />
+      <img
+        src="/image-product-desktop.jpg"
+        alt="product mobile"
+        className="max-w-full h-auto rounded-l-lg hidden md:block"
+      />
+      <div className="p-8 flex flex-col items-start justify-center gap-6 md:gap-8 text-grey">
+        <div>
+          <p className="text-preset-4">PERFUME</p>
+          <h1 className=" text-preset-1 text-black py-6">
+            Gabrielle Essence Eau De Parfum
+          </h1>
+          <p className="text-preset-3">
+            A floral, solar and voluptuous interpretation composed by Olivier
+            Polge, Perfumer-Creator for the House of CHANEL.
+          </p>
+        </div>
+        <div className="flex gap-4 items-center">
+          <h2 className="text-preset-1 text-green-500">$149.99</h2>
+          <p className="line-through text-preset-5">$169.99</p>
+        </div>
+        <button className="bg-green-500 flex gap-4 px-8 py-4 rounded-lg text-white text-preset-2 w-full justify-center items-center hover:bg-green-700 transition duration-300 cursor-pointer">
+          <img src="/icon-cart.svg" alt="icon cart" />
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Card;
